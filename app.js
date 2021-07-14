2256247e5ee8972574125b2c576c74a8c007c2ee
jcarreazo <jcarreazos.dev@gmail.com>
2021-07-14 00:31:03 -0500

    Adding jokes from the API

--- app.js
+++ app.js
@@ -1,17 +1,46 @@
 import VoiceRSS from "./VoiceRSS.js";
 const button = document.getElementById('button');
 const audioElement = document.getElementById('audio')
-function test() {
-    VoiceRSS.speech({
-        key: 'ebe27db905e44cbe92e4e55695e1bff1',
-        src: 'Hello, world!',
-        hl: 'en-us',
-        v: 'Linda',
-        r: 0,
-        c: 'mp3',
-        f: '44khz_16bit_stereo',
-        ssml: false
-    });
+var jokeSource;
+function test(apikey,word) {
+    try {
+        VoiceRSS.speech({
+            key: apikey,
+            src: word,
+            hl: 'en-us',
+            v: 'Linda',
+            r: 0,
+            c: 'mp3',
+            f: '44khz_16bit_stereo',
+            ssml: false
+        });
+    } catch (error) {
+        console.log(error)
+    }
 }
-test();
+
+
+// Get jokes fetching from the API
+
+async function getJokes() {
+    let joke='';
+    const apiURL = 'https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit';
+    try {
+        const response = await fetch(apiURL);
+        const data = await response.json();
+        if (data.setup) {
+            joke = `${data.setup} ... ${data.delivery}`
+        } else {
+            joke = data.joke;
+        }
+        test("ebe27db905e44cbe92e4e55695e1bff1",joke);
+    } catch (error) {
+        //Catch error
+        const e = new Error(error);
+        console.log(e);
+
+    }
+}
+getJokes();
+
 export default audioElement;
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -11,20 +11,71 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 
 var button = document.getElementById('button');
 var audioElement = document.getElementById('audio');
+var jokeSource;
 
-function test() {
-  _VoiceRSS["default"].speech({
-    key: 'ebe27db905e44cbe92e4e55695e1bff1',
-    src: 'Hello, world!',
-    hl: 'en-us',
-    v: 'Linda',
-    r: 0,
-    c: 'mp3',
-    f: '44khz_16bit_stereo',
-    ssml: false
-  });
+function test(apikey, word) {
+  try {
+    _VoiceRSS["default"].speech({
+      key: apikey,
+      src: word,
+      hl: 'en-us',
+      v: 'Linda',
+      r: 0,
+      c: 'mp3',
+      f: '44khz_16bit_stereo',
+      ssml: false
+    });
+  } catch (error) {
+    console.log(error);
+  }
+} // Get jokes fetching from the API
+
+
+function getJokes() {
+  var joke, apiURL, response, data, e;
+  return regeneratorRuntime.async(function getJokes$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          joke = '';
+          apiURL = 'https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit';
+          _context.prev = 2;
+          _context.next = 5;
+          return regeneratorRuntime.awrap(fetch(apiURL));
+
+        case 5:
+          response = _context.sent;
+          _context.next = 8;
+          return regeneratorRuntime.awrap(response.json());
+
+        case 8:
+          data = _context.sent;
+
+          if (data.setup) {
+            joke = "".concat(data.setup, " ... ").concat(data.delivery);
+          } else {
+            joke = data.joke;
+          }
+
+          test("ebe27db905e44cbe92e4e55695e1bff1", joke);
+          _context.next = 17;
+          break;
+
+        case 13:
+          _context.prev = 13;
+          _context.t0 = _context["catch"](2);
+          //Catch error
+          e = new Error(_context.t0);
+          console.log(e);
+
+        case 17:
+        case "end":
+          return _context.stop();
+      }
+    }
+  }, null, null, [[2, 13]]);
 }
 
-test();
+getJokes();
 var _default = audioElement;
 exports["default"] = _default;
